6f5e002bfd225c5ca7223e64b8de23ac46ce9675
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-13 14:59:44 +0100

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data(){
+        return {
+            title: "Lorem Ipsum"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+            title: "Lorem Ipsum"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
