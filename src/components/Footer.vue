913e663b358e4d8d592c7e12844da6a955223a94
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-13 17:58:38 +0100

    footer-center

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,49 +25,51 @@
             </div>
         </div>
         <div class="footer-center">
-            <div class="container clearfix">
-                <div class="first-col">
-                    <h3>dc comics</h3>
-                    <ul>
-                        <li><a>Characters</a></li>
-                        <li><a>Comics</a></li>
-                        <li><a>Movies</a></li>
-                        <li><a>TV</a></li>
-                        <li><a>Games</a></li>
-                        <li><a>Videos</a></li>
-                        <li><a>News</a></li>
-                    </ul>
-                    <h3>shop</h3>
-                    <ul>
-                        <li><a>Shop DC</a></li>
-                        <li><a>Shop DC Collectibles</a></li>
-                    </ul>
-                </div>
-                <div class="second-col">
-                    <h3>dc</h3>
-                    <ul>
-                        <li><a>Terms Of Use</a></li>
-                        <li><a>Privacy policy (New)</a></li>
-                        <li><a>Ad Choiches</a></li>
-                        <li><a>Advertising</a></li>
-                        <li><a>Jobs</a></li>
-                        <li><a>Subscriptions</a></li>
-                        <li><a>Talent Workshops</a></li>
-                        <li><a>CPSC Certificates</a></li>
-                        <li><a>Ratings</a></li>
-                        <li><a>Shop Help</a></li>
-                        <li><a>Contact Us</a></li>
-                    </ul>
-                </div>
-                <div class="third-col">
-                    <h3>sites</h3>
-                    <ul>
-                        <li><a>DC</a></li>
-                        <li><a>MAD Magazine</a></li>
-                        <li><a>DC Kids</a></li>
-                        <li><a>DC Universe</a></li>
-                        <li><a>DC Power Visa</a></li>
-                    </ul>
+            <div class="container">
+                <div class="background-logo clearfix">
+                    <div class="first-col">
+                        <h3>dc comics</h3>
+                        <ul>
+                            <li><a>Characters</a></li>
+                            <li><a>Comics</a></li>
+                            <li><a>Movies</a></li>
+                            <li><a>TV</a></li>
+                            <li><a>Games</a></li>
+                            <li><a>Videos</a></li>
+                            <li><a>News</a></li>
+                        </ul>
+                        <h3>shop</h3>
+                        <ul>
+                            <li><a>Shop DC</a></li>
+                            <li><a>Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="second-col">
+                        <h3>dc</h3>
+                        <ul>
+                            <li><a>Terms Of Use</a></li>
+                            <li><a>Privacy policy (New)</a></li>
+                            <li><a>Ad Choiches</a></li>
+                            <li><a>Advertising</a></li>
+                            <li><a>Jobs</a></li>
+                            <li><a>Subscriptions</a></li>
+                            <li><a>Talent Workshops</a></li>
+                            <li><a>CPSC Certificates</a></li>
+                            <li><a>Ratings</a></li>
+                            <li><a>Shop Help</a></li>
+                            <li><a>Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="third-col">
+                        <h3>sites</h3>
+                        <ul>
+                            <li><a>DC</a></li>
+                            <li><a>MAD Magazine</a></li>
+                            <li><a>DC Kids</a></li>
+                            <li><a>DC Universe</a></li>
+                            <li><a>DC Power Visa</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -118,36 +120,44 @@ export default {
 
     .footer-center {
         background-image: url(../assets/img/footer-bg.jpg);
-        .clearfix::after {
+        .container {
+            .clearfix::after {
             content: "";
             clear: both;
             display: table;
         }
-        .container {
-            padding: 2em 0 2em 0;
-            .first-col, .second-col, .third-col {
-            float: left;
-            width: 15%;
-            h3 {
-                text-transform: uppercase;
-                padding: 0.8em 0;
-                color: white;
-            }
-            ul {
-                list-style: none;
-                li {
-                    padding-bottom: 0.5em;
-                    font-size: 0.9em;
-                    a {
-                        text-decoration: none;
-                        color: $footerGreyText;
+            .background-logo {
+                padding: 2em 0 2em 0;
+                background-image: url('../assets/img/dc-logo-bg.png');
+                background-repeat: no-repeat;
+                background-position-x: 100%;
+                background-position-y: 50%;
+                .first-col, .second-col, .third-col {
+                    float: left;
+                    width: 15%;
+                    h3 {
+                        text-transform: uppercase;
+                        padding: 0.8em 0;
+                        color: white;
+                    }
+                    ul {
+                        list-style: none;
+                        li {
+                            padding-bottom: 0.5em;
+                            font-size: 0.9em;
+                            a {
+                                text-decoration: none;
+                                color: $footerGreyText;
+                            }
+                        }
                     }
                 }
+                
             }
+            
         }
-        }
-        
     }
+        
 
     .footer-bottom {
         background-color: $footerBottomBackground;
