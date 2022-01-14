696d53298659b78d80f05ef3096e5e64205c2ee6
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-14 15:44:13 +0100

    add jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="background">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/style.scss";
+    .background {
+        background-image: url(../assets/img/jumbotron.jpg);
+        background-size: cover;
+        height: 400px;
+    }
+</style>
