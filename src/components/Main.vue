21b76391637281d9b8b1999f1eb9b8c07c4490d7
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-14 15:44:23 +0100

    add thumb in main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,105 @@
 <template>
     <main>
         <div class="container">
-            <h1>--> Content goes here</h1>
+            <div class="title">
+                <span>current series</span>
+            </div>
+            <div class="thumbs">
+                <Thumb v-for="(thumb, index) in thumbs" :key="index" :img="thumb.thumb" :alt="thumb.series" :title="thumb.series"/>
+            </div>
+            <div class="bottom-container">
+                <div class="load-more">
+                    <span>load more</span>
+                </div>
+            </div>
         </div>
     </main>
 </template>
 
 <script>
+import Thumb from "./Thumb.vue";
+
 export default {
-    name: 'Header',
+    name: 'Main',
+    components: {
+        Thumb,
+    },
     data(){
         return {
-            title: "Lorem Ipsum"
+            thumbs: [
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    price: "$19.99",
+                    series: "Action Comics",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    price: "$3.99",
+                    series: "American Vampire 1976",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    price: "$16.99",
+                    series: "Aquaman",
+                    type: "graphic novel"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    price: "$2.99",
+                    series: "Batgirl",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    price: "$3.99",
+                    series: "Batman",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    price: "$2.99",
+                    series: "Batman Beyond",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    price: "$3.99",
+                    series: "Batman/Superman",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    price: "$4.99",
+                    series: "Batman/Superman Annual",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    price: "$5.99",
+                    series: "Batman: The Joker War Zone",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    price: "$6.99",
+                    series: "Batman: Three Jokers",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    price: "$4.99",
+                    series: "Batman: White Knight Presents: Harley Quinn",
+                    type: "comic book"
+                },
+                {
+                    thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    price: "$16.99",
+                    series: "Catwoman",
+                    type: "graphic novel"
+                }
+            ]
         }
     }
 }
@@ -20,15 +108,37 @@ export default {
 <style lang="scss" scoped>
     @import "../assets/scss/style.scss";
     main {
-        height: 100px;
         background-color: $mainBackground;
         .container {
-            display: flex;
-            align-items: center;
-            height: 100%;
-            h1 {
-            color: white;
-        }
+            padding: 2em 0;
+            position: relative;
+            .title {
+                text-transform: uppercase;
+                color: white;
+                font-weight: 600;
+                padding: 1em;
+                background-color: $lightblue;
+                position: absolute;
+                top: -5%;
+                }
+            .thumbs {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 2.4em;
+            }
+            .bottom-container {
+                display: flex;
+                justify-content: center;
+                margin-top: 2em;
+                .load-more {
+                text-transform: uppercase;
+                color: white;
+                font-weight: 600;
+                padding: 1em 3em;
+                background-color: $lightblue;
+                display: inline-block;
+                }
+            }
         }
     }
 </style>
---
+++ src/components/Thumb.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="thumb">
+        <div class="img-container">
+            <img :src="img" :alt="alt">
+        </div>
+        <span class="subtitle">{{title}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Thumb",
+    props: ["img", "alt", "title", "type", "price"],
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.thumb {
+    width: 200px;
+    .img-container {
+        height: 200px;
+        width: 200px;
+        overflow: hidden;
+        margin-bottom: 1em;
+        img {
+            width: 100%;
+        }
+    }
+    .subtitle {
+        color: white;
+        text-transform: uppercase;
+        font-size: 0.8em;
+    }
+}
+</style>
